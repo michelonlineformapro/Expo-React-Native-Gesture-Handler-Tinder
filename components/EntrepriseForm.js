4d584402a6f1f2960b34c4efd9e458168222a0d4
michelonlineformapro <m.michel@onlineformapro.com>
2021-11-09 19:53:47 +0100

    Ajout des commentaire pour react-hook-form

--- components/EntrepriseForm.js
+++ components/EntrepriseForm.js
@@ -3,9 +3,13 @@ import {View, TextInput, TouchableOpacity,ScrollView, Text, StyleSheet} from "re
 import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
 
-
+//La fonction (composant) prend l'objet navigation en paramètre pour acceder a la fonction navigate
+//<TouchableOpacity style={styles.buttonValider} onPress={() => navigation.navigate("HomeSwipes")}>
 function EntrepriseForm({navigation}){
 
+
+    //Creation d'un objet qui reprend toutes les valeurs de la collection entreprise (db.json)
+    //Toutes les valeurs sont initialisées et stockée dans l'objet initEntrepriseData{}
     const initEntrepriseData = {
         id:null,
         nom:"",
@@ -23,14 +27,27 @@ function EntrepriseForm({navigation}){
     const [valider, setValider] = useState(false);
 
     //Test de react-hook-form
-    const { control, handleSubmit } = useForm();
+    //useForm =
+    /*
+    useForm est un hook (crochet) personnalisé pour gérer facilement
+     les formulaires. Il prend des arguments optionnels .
+     https://react-hook-form.com/api/useform
+     */
+    const control = useForm();
+    const handleSubmit = useForm();
+
+    //SON EQUIVALENT ES5
+    //const { control, handleSubmit } = useForm();
+
+    //la constante onSubmit = function data()
     const onSubmit =  data => {
         //On creer un nouvelle objet similaire a initEntrepriseData pour le remplir
-        //Chaque nouvelle valeur entrer dans le formulaire est egale a la valeur de l'objet initial
-        //Creation de la requète http axios + methode + url + promesse
+        //Chaque nouvelle valeur entrée dans le formulaire est egale a la valeur de l'objet initial
+        //Creation de la requète http axios + methode + url (backend json-server) + promesse
         axios.post("http://localhost:3000/entreprises", data)
+            //Creation de la promesse (.then() retourne resolve et .catch() retourne reject
             .then(response => {
-                //On rempli l'objet entreprise avec le mutatuer setEntreprise du hook
+                //On rempli l'objet entreprise avec le mutateur (setter) setEntreprise du hook
                 setEntreprise({
                     id: response.data.id,
                     nom: response.data.nom,
@@ -42,21 +59,29 @@ function EntrepriseForm({navigation}){
                 });
                 //Le booleen valider passe a true
                 setValider(true);
+                //Debug pour tester le retour des valeur de la collection json
                 console.log(response.data);
+                //On retourne la fonction data
                 return data
-                //Debug
-
             })
-            //Sinon on declenche une erreur
+            //Sinon on declenche une erreur + un debug
             .catch(err => {
                 console.log("Erreur lors de la creation du profil entreprise ! " + err);
             });
     }
 
+    //1- si le hook valider retourne true = on affiche le block de code (bouton pour acceder au swipe)
+    //Sinon on affiche le formulaire de creation d'une entreprise
+    /*
+    if(valider === true){
+        block1 = bouton pour acceder au swipe
+    }else{
+        block 2 = formulaire de creation d'une entreprise
+    }
+     */
 
     return(
         <ScrollView>
-
             {valider ? (
                 <View style={styles.container}>
                     <Text style={styles.title}>Vous êtes inscrits</Text>
@@ -71,20 +96,38 @@ function EntrepriseForm({navigation}){
                     <Text style={styles.title}>
                         ENTREPRISE
                     </Text>
+                    {/*Composant de react-hook-form <Controller>*/}
                     <Controller
+                        {/*
+                            Cet element indique que le formualire est un composant controllé: https://fr.reactjs.org/docs/forms.html
+                        */}
                         control={control}
+                        {/*
+                            Ceci est un accessoire de rendu . Une fonction qui renvoie un élément React et offre la possibilité d'attacher des événements
+                            et une valeur dans le composant. Cela simplifie l'intégration avec des composants contrôlés externes avec des noms
+                            d'accessoires non standard. Fournit , , , et la composante des enfants,
+                            et aussi un objet qui contient l' état d'entrée spécifique. onChange onBlur name ref value field State
+                        */}
                         render={({
                                      field:{onChange, onBlur, value}
                                  }) => (
                             <TextInput
                                 style={styles.input}
                                 onBlur={onBlur}
+                                {/*
+                                    La validation se déclenchera sur l' changement d' événement à chaque entrée et conduira à plusieurs re-rendus.
+                                    Attention : cela a souvent un impact significatif sur les performances.
+                                */}
                                 onChangeText={value => onChange(value)}
                                 placeholder={'NOM'}
                                 placeholderTextColor="white"
+                                {/*
+                                La valeur du champ. elle est envoyée a onChangeText={value => onChange(value)}
+                                */}
                                 value={value}
                             />
                         )}
+                        {/*Ciblez une seule entrée par son nom.*/}
                         name="nom"/>
 
 
@@ -171,6 +214,8 @@ function EntrepriseForm({navigation}){
                         )}
                         name="password_repeat"/>
 
+                    {/*Au click on lance handleSubmit = Cette fonction recevra les données du formulaire si la validation du formulaire est réussie.*/}
+                    {/*Cette fonction prend en paramètre la const onSubmit = fonction data soit :  Creation de la requète http axios + methode + url (backend json-server) + promesse*/}
                     <TouchableOpacity style={styles.buttonValider} onPress={handleSubmit(onSubmit)}>
                         <Text style={styles.textBtnValider}>
                             VALIDER
